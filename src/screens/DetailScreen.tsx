ae02ab5cfa5e1180d308ec627989e5b7a56e1ca2
Alexis <alexiszanotti@gmail.com>
2024-04-19 22:01:05 +0200

    Refactor DetailScreen component to use createBottomTabNavigator in DetailScreen.tsx

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,75 +1,50 @@
-import {
-  StyleSheet,
-  Image,
-  View,
-  ScrollView,
-  Pressable,
-  useWindowDimensions,
-} from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { RootStackParams } from '../navigation/Navigation';
+import { CharacterComics } from '../components/CharacterComics';
 import { CharacterDetail } from '../components';
+import { globalStyles } from '../theme/theme';
+import { useContext } from 'react';
+import { ThemeContext } from '../context/ThemeContext';
 
 interface Props extends StackScreenProps<RootStackParams, 'Detail'> {}
 
 export const DetailScreen = ({ route, navigation }: Props) => {
-  const { height: screenHeight } = useWindowDimensions();
-  const { name, description, thumbnail, id } = route.params;
+  const Tab = createBottomTabNavigator();
 
-  const uri = `${thumbnail.path}/portrait_incredible.${thumbnail.extension}`;
+  const { colors } = useContext(ThemeContext);
 
   return (
-    <ScrollView>
-      <Pressable style={styles.backButton} onPress={() => navigation.goBack()}>
-        <Ionicons name="arrow-back" size={30} color="white" />
-      </Pressable>
-      <View style={[styles.imageContainer, { height: screenHeight * 0.6 }]}>
-        <Image source={{ uri }} style={styles.image} />
-      </View>
-      <View style={styles.marginContainer}>
-        <CharacterDetail character={route.params} />
-      </View>
-    </ScrollView>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: 'red',
+        tabBarInactiveTintColor: colors.subTitle,
+        tabBarStyle: {
+          backgroundColor: colors.background,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Detail"
+        children={() => <CharacterDetail character={route.params} />}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="information-circle" color={color} size={size} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Comics"
+        component={CharacterComics}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="book" color={color} size={size} />
+          ),
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
   );
 };
-
-const styles = StyleSheet.create({
-  imageContainer: {
-    width: '100%',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 10,
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 10,
-    elevation: 10,
-  },
-  image: {
-    flex: 1,
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-  },
-  marginContainer: {
-    marginHorizontal: 20,
-    paddingVertical: 20,
-  },
-  backButton: {
-    position: 'absolute',
-    top: 40,
-    left: 20,
-    width: 50,
-    height: 50,
-    zIndex: 1,
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    borderRadius: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 10,
-    },
-  },
-});
