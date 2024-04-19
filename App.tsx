3ce34cfb3ed366ebcab5250d19008ee31a36884a
Alexis <alexiszanotti@gmail.com>
2024-04-19 21:59:18 +0200

    Add Header component to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { Navigation } from './src/navigation/Navigation';
 import { ThemeProvider } from './src/context/ThemeContext';
+import { Header } from './src/components';
 
 export default function App() {
   return (
     <ThemeProvider>
+      <Header />
       <Navigation />
     </ThemeProvider>
   );
